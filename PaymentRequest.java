b4cbef15e6b07328d348c6899396eb80c8802649
João Carlos Vieira Filho <me.contrate.aero@gmail.com>
2026-02-16 00:13:15 -0300

    mocas

---
+++ PaymentRequest.java
@@ -0,0 +1,31 @@
+package com.eliasnogueira.paymentservice.dto;
+
+import com.eliasnogueira.paymentservice.model.enums.PaymentSource;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Builder
+@Data
+public class PaymentRequest {
+
+    @NotNull(message = "Payer ID is required")
+    private UUID payerId;
+
+    @NotNull(message = "Payment source is required")
+    private PaymentSource paymentSource;
+
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be positive")
+    private BigDecimal amount;
+
+    public PaymentRequest(UUID payerId, PaymentSource paymentSource, BigDecimal amount) {
+        this.payerId = payerId;
+        this.paymentSource = paymentSource;
+        this.amount = amount;
+    }
+}
